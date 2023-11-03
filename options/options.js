8031301e1392b1bd8ad4bfc3ed347190fd5b8b64
Crowdbotics <team@crowdbotics.com>
2023-11-03 18:24:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quantum-quasar-44322",
+  url: "https://quantum-quasar-44322.botics.co",
+  api: "https://quantum-quasar-44322.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
